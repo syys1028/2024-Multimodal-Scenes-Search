498298b2fc81dccfb1bc2c29bc270e64687f44f3
서연 <107555293+syys1028@users.noreply.github.com>
2024-09-18 13:37:59 +0900

    Update db_connect.py

--- Capstone/db_connect.py
+++ Capstone/db_connect.py
@@ -2,7 +2,7 @@ import pymysql
 
 # 데이터베이스에 연결하는 함수
 def dbconnect():
-    conn = pymysql.connect(host='202.31.147.129', user='jisung', password='Wldnjs981212@@', db='movie_hackerton',port=13306 ,charset='utf8')
+    conn = pymysql.connect(host='my_ip', user='root', password='my_pw', db='movie_hackerton',port=13306 ,charset='utf8')
     return conn
 
 # 데이터베이스에서 모든 데이터를 검색하는 함수
